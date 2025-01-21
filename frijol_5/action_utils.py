df5d09ab7422f7341f4cb18de7bdd6a470b4d9cb
Tomas <tomascr@mit.edu>
2025-01-21 12:58:43 -0500

    Added Frijol_5. Frijol_4 in scrimmage as frijol-4.1

---
+++ frijol_5/action_utils.py
@@ -0,0 +1,86 @@
+import random
+import numpy as np
+from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction
+from helper_bot import FrijolBot
+import eval7
+
+def CheckFold(bot: FrijolBot):
+    """
+    Determines the appropriate action (Check or Fold) based on the available legal actions.
+
+    Args:
+        bot (FrijolBot): The current bot in the game, which includes the legal actions.
+
+    Returns:
+        Action: Returns a CheckAction if it is in the list of legal actions, otherwise returns a FoldAction.
+    """
+    if CheckAction in bot.get_legal_actions():
+        return CheckAction()
+    return FoldAction()
+
+
+def CheckCall(bot: FrijolBot):
+    """
+    Determines whether to perform a check or call action based on the available legal actions.
+
+    Args:
+        bot (FrijolBot): The current bot in the game, which includes the legal actions.
+
+    Returns:
+        Action: The CheckAction if it is in the list of legal actions, otherwise the CallAction.
+    """
+    if CheckAction in bot.get_legal_actions():
+        return CheckAction()
+
+    return CallAction()
+
+
+def RaiseCheckCall(bot: FrijolBot, raise_amount: float):
+    """
+    Determines the appropriate action (Raise, Check, or Call) based on the given parameters.
+
+    Args:
+        bot (FrijolBot): The current bot in the game, which includes the legal actions.
+        raise_amount (float): The amount to raise by.
+
+    Returns:
+        Action: A RaiseAction with the appropriate raise amount if raising is legal, 
+                otherwise a CheckCall action.
+    """
+    if RaiseAction in bot.get_legal_actions():
+        min_raise, max_raise = bot.get_raise_bounds()
+        raise_amount = min(raise_amount, max_raise)
+        raise_amount = max(raise_amount, min_raise)
+        return RaiseAction(int(raise_amount))
+
+    return CheckCall(bot)
+
+
+def mixed_strategy(bot: FrijolBot, fold_probability: float, call_probability: float, raise_amount: float = 1):
+    """
+    Does a randomized selection of actions depending on input probabilities
+
+    Inputs:
+    legal actions: The set of legal actions
+    my_pip: Your total contribution of chips this betting round
+    round_state: Round state object from RoundState
+    checkfold: The probability with which it will choose check-fold action
+    checkcall: The probabiity with which it will choose check-call action
+    1-checkfold-checkcall will be the probability of raising.
+    raise_amount: How much it will raise given that it chooses raise.
+
+    Output: An action
+    """
+
+    action_probability = random.random()
+
+    if action_probability < fold_probability:
+        return CheckFold(bot)
+    elif action_probability < fold_probability + call_probability:
+        return CheckCall(bot)
+
+    min_raise, max_rasie = bot.get_raise_bounds()
+    std_dev = (raise_amount - min_raise) / 10
+    raise_amount = np.random.normal(raise_amount, std_dev)
+
+    return RaiseCheckCall(bot, raise_amount)
---
+++ frijol_5/helper_bot.py
@@ -0,0 +1,102 @@
+from skeleton.bot import Bot
+import skeleton.states as states
+from io_utils import*
+import numpy as np
+
+class FrijolBot(Bot):
+    def __init__(self):
+        self.game_state = None
+        self.round_state = None
+        self.terminal_state = None
+        self.active = None
+        self.opponent_bounty_distribution = np.zeros(13)
+        (self.BTN_opening_range, 
+         self.BB_call_range_vs_open, 
+         self.BB_3bet_range_vs_open, 
+         self.BTN_call_range_vs_3bet, 
+         self.BTN_4bet_range_vs_3bet, 
+         self.BB_call_range_vs_4bet, 
+         self.BB_5bet_range_vs_4bet, 
+         self.BB_raise_range_vs_limp)=read_starting_ranges("my_starting_ranges.csv")
+        print(np.shape(self.BB_3bet_range_vs_open))
+    
+    def get_bankroll(self):
+        return self.game_state.bankroll
+    
+    def get_game_clock(self):
+        return self.game_state.game_clock
+    
+    def get_round_num(self):
+        return self.game_state.round_num
+
+    def get_rounds_left(self):
+        return states.NUM_ROUNDS - self.game_state.round_num + 1
+
+    def get_big_blind(self):
+        return bool(self.active)
+    
+    def get_previous_state(self):
+        return self.terminal_state.previous_state
+
+    def get_legal_actions(self):
+        return self.round_state.legal_actions()
+    
+    def get_street(self):
+        if self.round_state is not None:
+            return self.round_state.street
+        return self.get_previous_state().street
+    
+    def get_my_cards(self):
+        if self.round_state is not None:
+            return self.round_state.hands[self.active]
+        return self.get_previous_state().hands[self.active]
+
+    def get_board_cards(self):
+        if self.round_state is not None:
+            return self.round_state.deck[:self.get_street()]
+        return self.get_previous_state().deck[:self.get_street()]
+    
+    def get_opponent_cards(self):
+        return self.get_previous_state().hands[1-self.active]
+    
+    def get_my_pip(self):
+        return self.round_state.pips[self.active]
+
+    def get_opponent_pip(self):
+        return self.round_state.pips[1-self.active]
+    
+    def get_my_stack(self):
+        return self.round_state.stacks[self.active]
+    
+    def get_opponent_stack(self):
+        return self.round_state.stacks[1-self.active]
+    
+    def get_continue_cost(self):
+        return self.get_opponent_pip() - self.get_my_pip()
+    
+    def get_my_bounty(self):
+        return self.round_state.bounties[self.active]
+    
+    def get_my_contribution(self):
+        return states.STARTING_STACK - self.get_my_stack()
+    
+    def get_opponent_contribution(self):
+        return states.STARTING_STACK - self.get_opponent_stack()
+
+    def get_my_delta(self):
+        return self.terminal_state.deltas[self.active]
+    
+    def get_my_bounty_hit(self):
+        return self.terminal_state.bounty_hits[self.active]
+    
+    def get_opponent_bounty_hit(self):
+        return self.terminal_state.bounty_hits[1-self.active]
+    
+    def get_opponent_bounty_distribution(self):
+        return self.opponent_bounty_distribution
+    
+    def get_raise_bounds(self):
+        return self.round_state.raise_bounds()
+    
+    def get_opponent_range(self):
+        return self.opponent_range
